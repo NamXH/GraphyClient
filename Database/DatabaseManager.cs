4c2c254ead0ca1f01d0d72d240b639857d574fd3
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2016-03-17 15:47:31 -0600

    Add DB manager

---
+++ Database/DatabaseManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SQLite;
+
+namespace GraphyClient
+{
+    public class DatabaseManager
+    {
+        public SQLiteConnection DbConnection { get; private set; }
+
+        private string _dbName;
+
+        public string DbName
+        {
+            get { return _dbName; }
+        }
+
+        public DatabaseManager(string dbName)
+        {
+            _dbName = dbName;
+        }
+
+        public 
+    }
+}
+
